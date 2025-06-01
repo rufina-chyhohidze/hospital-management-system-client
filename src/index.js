9d14e9f7498dd118565a877f0bc0d73b1bdd9a7b
rufinachyhohidze <rufina.chyhohidze@student.kdg.be>
2025-06-01 14:46:02 +0200

    exam

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './css/style.scss'
-//import {addTestEventListener} from './js/hospitals.js'
 import 'bootstrap'
 import './js/hospitals.js'
 
--- src/js/fetch-calls.js
+++ src/js/fetch-calls.js
@@ -1,17 +1,13 @@
 const BASE_URL = 'http://localhost:8080';
 
-/**
- * Fetch hospital by ID
- */
+
 export async function fetchHospital(id) {
     const res = await fetch(`${BASE_URL}/api/hospitals/${id}`);
     if (!res.ok) return null;
     return await res.json();
 }
 
-/**
- * PATCH hospital
- */
+
 export async function updateHospital(id, name, address, date) {
     const res = await fetch(`${BASE_URL}/api/hospitals/${id}`, {
         method: 'PATCH',
